4b44584fd578eebe13ca755df00ac616c215d02c
Howni <58996617+hownioni@users.noreply.github.com>
2026-02-06 17:44:11 -0600

    Third time's the charm

--- lua/Arduino-Nvim/init.lua
+++ lua/Arduino-Nvim/init.lua
@@ -549,7 +549,7 @@ function M.monitor()
 
 			-- Start the actual monitor in the new buffer
 			vim.fn.jobstart(serial_command, {
-				cwd = vim.fn.shellescape(vim.fn.expand("%:p:h")),
+				cwd = '"' .. vim.fn.expand("%:p:h") .. '"',
 				term = true,
 				on_exit = function(_, exit_code)
 					if exit_code ~= 0 and vim.api.nvim_buf_is_valid(term_buf) then
